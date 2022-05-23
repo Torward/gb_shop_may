3c8764fe400b669b69f364d840372be7c2bee015
Torward <lomovfg@gmail.com>
2022-05-23 18:07:16 +1000

    Initial commit

---
+++ src/main/java/ru/lomov/gb_shop_may/GbShopMayApplication.java
@@ -0,0 +1,13 @@
+package ru.lomov.gb_shop_may;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbShopMayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbShopMayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lomov/gb_shop_may/GbShopMayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lomov.gb_shop_may;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbShopMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
